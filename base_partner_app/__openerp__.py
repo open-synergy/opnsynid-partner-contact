d3b7e43ea08a69617c968560e44180a4e2946df6
mike <viriyananda.michael@gmail.com>
2016-12-21 14:23:43 +0700

    Adding new module base_partner_app

---
+++ base_partner_app/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Base Partner Application",
+    "version": "8.0.1.0.0",
+    "summary": "Base module to manage partner",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["base"],
+    "data": ["views/res_partner_view.xml"],
+}
