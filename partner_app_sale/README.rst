5c8c36b5611ca1711e37e8e356c5be8fc98956e1
Andhitia Rama <andhitia.r@gmail.com>
2016-12-30 17:52:50 +0700

    Finishing touch

--- partner_app_sale/README.rst
+++ partner_app_sale/README.rst
@@ -2,9 +2,9 @@
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-============================
-Partner Application For Sale
-============================
+===============================================
+Partner Management Application - sale Extension
+===============================================
 
 Module to hide menu partner.
 
--- partner_app_sale/__openerp__.py
+++ partner_app_sale/__openerp__.py
@@ -2,15 +2,19 @@
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Partner Application Sale",
+    "name": "Partner Management Application - sale",
     "version": "8.0.1.0.0",
-    "summary": "Hide menu partner",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "auto_install": True,
-    "depends": ["partner_app"],
-    "data": ["views/res_partner_view.xml"],
+    "depends": [
+        "partner_app",
+        "sale",
+        ],
+    "data": [
+        "views/res_partner_view.xml",
+        ],
 }
