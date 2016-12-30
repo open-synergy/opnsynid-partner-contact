29a98c957113e385c2f7f3f0cf3f8ac9e613d82a
Andhitia Rama <andhitia.r@gmail.com>
2016-12-30 16:19:18 +0700

    [IMP] Better readme

--- partner_app/__openerp__.py
+++ partner_app/__openerp__.py
@@ -2,8 +2,8 @@
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Base Partner Application",
-    "version": "8.0.1.0.0",
+    "name": "Partner's Management Application",
+    "version": "8.0.1.0.1",
     "summary": "Base module to manage partner",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
