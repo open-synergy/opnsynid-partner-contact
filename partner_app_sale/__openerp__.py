d97b82409cabdd4a435365f8b93ad78263fcc188
mike <viriyananda.michael@gmail.com>
2016-12-30 11:02:56 +0700

    Adding new module partner_app_sale

---
+++ partner_app_sale/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Application Sale",
+    "version": "8.0.1.0.0",
+    "summary": "Hide menu partner",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": ["partner_app"],
+    "data": ["views/res_partner_view.xml"],
+}
