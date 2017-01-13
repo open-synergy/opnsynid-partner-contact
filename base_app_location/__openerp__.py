9c2f18c81df4f54421ecf56deda8f4a11d04327f
mike <viriyananda.michael@gmail.com>
2017-01-13 10:15:49 +0700

    Adding new module base_app_location

---
+++ base_app_location/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Management Application - Base Location",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "partner_app",
+        "base_location",
+        ],
+    "data": [
+        "views/res_partner_view.xml",
+        ],
+}
