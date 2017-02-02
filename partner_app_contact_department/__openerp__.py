4769ac6011d9474003665a685d5b3bfb6f6e4a55
mike <viriyananda.michael@gmail.com>
2017-02-02 12:49:03 +0700

    Adding new module partner_app_contact_department

---
+++ partner_app_contact_department/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Management Application - Contact Department",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "partner_app",
+        "partner_contact_department",
+        ],
+    "data": [
+        "views/res_partner_view.xml",
+        ],
+}
