1b5162e8edda2b029858e4ba823f1b7d7be0c61d
Andhitia Rama <andhitia.r@gmail.com>
2017-06-11 00:31:10 +0700

    [8.0.2.0.0] partner_app
    1. Add feature to mark/unmark as customer/supplier

---
+++ partner_app/models/res_partner.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.multi
+    def button_toggle_customer(self):
+        for partner in self:
+            partner._toggle_customer()
+
+    @api.multi
+    def button_toggle_supplier(self):
+        for partner in self:
+            partner._toggle_supplier()
+
+    @api.multi
+    def _toggle_customer(self):
+        self.ensure_one()
+        criteria = [
+            "|",
+            ("id", "=", self.id),
+            ("commercial_partner_id", "=", self.id),
+        ]
+        self.env["res.partner"].search(criteria).write({
+            "customer": not self.customer,
+        })
+
+    @api.multi
+    def _toggle_supplier(self):
+        self.ensure_one()
+        criteria = [
+            "|",
+            ("id", "=", self.id),
+            ("commercial_partner_id", "=", self.id),
+        ]
+        self.env["res.partner"].search(criteria).write({
+            "supplier": not self.supplier,
+        })
