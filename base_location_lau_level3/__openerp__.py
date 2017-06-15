e11b5a1f384b80a77d4711a6aafd44eb18158f5b
Andhitia Rama <andhitia.r@gmail.com>
2017-06-15 22:03:29 +0700

    [8.0.1.0.0] base_location_lau_level3

---
+++ base_location_lau_level3/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "LAU Level 3",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base_location_lau_partner_view",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
---
+++ base_location_lau_level3/models/res_partner.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Andhitia Rama
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    lau3_id = fields.Many2one(
+        'res.partner.lau',
+        'Local Admin. Unit 3',
+        domain=[('level', '=', 3)])
+
+    @api.multi
+    @api.onchange('lau2_id')
+    def _onchange_lau2(self):
+        for p in self:
+            if p.lau3_id and p.lau3_id.parent_id != p.lau2_id:
+                p.lau3_id = None
