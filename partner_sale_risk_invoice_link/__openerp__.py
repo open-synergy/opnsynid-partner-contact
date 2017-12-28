1a15e458f651509f222ff146088dd8108bb3bfb8
Andhitia Rama <andhitia.r@gmail.com>
2017-12-29 06:18:49 +0700

    [8.0.2.0.0] partner_sale_risk_invoice_link
    Add extra depend fields to computed fields

--- partner_sale_risk_invoice_link/__openerp__.py
+++ partner_sale_risk_invoice_link/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Link Between Partner Sale Risk and Picking Invoice Link",
-    "version": "8.0.1.0.1",
+    "version": "8.0.2.0.0",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
