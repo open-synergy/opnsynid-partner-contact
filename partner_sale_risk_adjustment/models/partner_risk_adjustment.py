ed7286e93bdaa829bd32dd2e4dcf818022c35a39
Andhitia Rama <andhitia.r@gmail.com>
2018-03-23 20:49:37 +0700

    [8.0.1.0.0] partner_sale_risk_adjustment

---
+++ partner_sale_risk_adjustment/models/partner_risk_adjustment.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class PartnerRiskAdjustment(models.Model):
+    _inherit = "partner.risk_adjustment"
+
+    risk_sale_order = fields.Float(
+        string="Total Sale Order Adjustment",
+        required=True,
+        default=0.0,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
---
+++ partner_sale_risk_adjustment/models/res_partner.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.multi
+    @api.depends(
+        "risk_adjustment_ids",
+    )
+    def _compute_sale_risk_adjustment(self):
+        for partner in self:
+            risk_sale_order_adjustment = 0.0
+            for adj in partner.risk_adjustment_ids.filtered(
+                    lambda r: r.state == "done"):
+                risk_sale_order_adjustment += adj.risk_sale_order
+            partner.risk_sale_order_adjustment = \
+                risk_sale_order_adjustment
+
+    @api.multi
+    @api.depends(
+        "sale_order_ids", "sale_order_ids.invoice_pending_amount",
+        "child_ids.sale_order_ids",
+        "child_ids.sale_order_ids.invoice_pending_amount",
+        "risk_adjustment_ids", "risk_adjustment_ids.state",
+    )
+    def _compute_risk_sale_order(self):
+        _super = super(ResPartner, self)
+        _super._compute_risk_sale_order()
+        for partner in self:
+            partner.risk_sale_order += \
+                partner.risk_sale_order_adjustment
+
+    risk_sale_order_adjustment = fields.Float(
+        string="Sale Order Adjustment",
+        compute="_compute_sale_risk_adjustment",
+        store=False,
+        readonly=True,
+    )
+    risk_sale_order = fields.Float(
+        default="_compute_risk_sale_order",
+    )
