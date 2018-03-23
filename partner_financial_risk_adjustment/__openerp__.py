a6131504f925be8ab26fec48dee972ae89bad068
Andhitia Rama <andhitia.r@gmail.com>
2018-03-23 18:26:52 +0700

    [8.0.1.0.0] partner_financial_risk_adjustment

---
+++ partner_financial_risk_adjustment/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Financial Risk Adjustment",
+    "version": "8.0.4.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "partner_financial_risk",
+    ],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "security/ir.model.access.csv",
+        "views/partner_risk_adjustment_views.xml",
+        "views/res_partner_views.xml",
+    ],
+}
