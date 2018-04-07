0c3bc44f0647bdbecf6f58c40452631f8b1ec997
Andhitia Rama <andhitia.r@gmail.com>
2018-04-07 15:21:05 +0700

    [8.0.1.0.0] partner_go_public

---
+++ partner_go_public/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Go Public Information",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "APGL-3",
+    "category": "Partner Management",
+    "summary": "Store Partner's Go Public Information",
+    "depends": [
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
