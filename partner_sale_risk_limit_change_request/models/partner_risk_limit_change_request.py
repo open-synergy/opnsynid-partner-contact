d3962060095a4b20266e2a6c728165fe4affc052
Andhitia Rama <andhitia.r@gmail.com>
2018-04-17 12:43:23 +0700

    [8.0.1.0.0] partner_sale_risk_limit_change_request

---
+++ partner_sale_risk_limit_change_request/models/partner_risk_limit_change_request.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class PartnerRiskLimitChangeRequest(models.Model):
+    _inherit = "partner.risk_limit_change_request"
+
+    risk_sale_order = fields.Float(
+        string="Sale Order Risk",
+        required=True,
+        default=0.0,
+        copy=False,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+
+    @api.multi
+    def _prepare_partner_data(self):
+        self.ensure_one()
+        _super = super(PartnerRiskLimitChangeRequest, self)
+        result = _super._prepare_partner_data()
+        if self.risk_sale_order:
+            result.update(
+                {"risk_sale_order_limit": self.risk_sale_order})
+        return result
