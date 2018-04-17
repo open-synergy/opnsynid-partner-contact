b0a6be08bcc83a22f0b44909ab565bef04aa759c
Andhitia Rama <andhitia.r@gmail.com>
2018-04-17 13:07:04 +0700

    [8.0.1.0.0] partner_single_sale_risk_limit_change_request

---
+++ partner_single_sale_risk_limit_change_request/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Single Sale Risk Limit Change Request",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "partner_single_sale_risk",
+        "partner_financial_risk_limit_change_request",
+    ],
+    "data": [
+        "views/partner_risk_limit_change_request_views.xml",
+    ],
+}
