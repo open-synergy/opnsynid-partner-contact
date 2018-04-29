341ae2c1b630f65030ee763737c6b632c9e560fe
Andhitia Rama <andhitia.r@gmail.com>
2018-04-29 18:17:36 +0700

    [8.0.2.0.0] partner_app_risk_limit_change_request
    * Adds configuration
    * Rearrange menu

--- partner_app_risk_limit_change_request/__openerp__.py
+++ partner_app_risk_limit_change_request/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner App - Risk Limit Change Request Extension",
-    "version": "8.0.1.0.0",
+    "version": "8.0.2.0.0",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -11,10 +11,11 @@
     "installable": True,
     "auto_install": True,
     "depends": [
-        "partner_app",
+        "partner_app_partner_risk",
         "partner_financial_risk_limit_change_request",
     ],
     "data": [
+        "views/partner_risk_config_setting_views.xml",
         "views/res_partner_view.xml",
     ],
 }
