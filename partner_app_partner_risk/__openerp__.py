bbc614467bb15a5c36060d2919098c51c2bc4614
Andhitia Rama <andhitia.r@gmail.com>
2018-04-29 16:54:45 +0700

    [8.0.1.0.0] partner_app_partner_risk

---
+++ partner_app_partner_risk/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner App - Partner Risk Extension",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "partner_app",
+        "partner_financial_risk",
+    ],
+    "data": [
+        "menu.xml",
+        "views/partner_risk_config_setting_views.xml",
+    ],
+}
