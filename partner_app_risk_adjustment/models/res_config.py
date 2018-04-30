6c51f5ef6a7dd6878efa71a7426dc168c5e2da81
Andhitia Rama <andhitia.r@gmail.com>
2018-04-30 09:45:32 +0700

    [8.0.1.0.0] partner_app_risk_adjustment

---
+++ partner_app_risk_adjustment/models/res_config.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResConfig(models.TransientModel):
+    _inherit = "partner.partner_risk_config_setting"
+
+    partner_risk_adjustment_sequence_id = fields.Many2one(
+        string="Partner Risk Adjustment Sequence",
+        comodel_name="ir.sequence",
+        related="company_id.partner_risk_adjustment_sequence_id",
+        store=False,
+    )
+    partner_risk_adjustment_confirm_grp_ids = fields.Many2many(
+        string="Allowed To Confirm Partner Risk Adjustment",
+        comodel_name="res.groups",
+        related="company_id.partner_risk_adjustment_confirm_grp_ids",
+        store=False,
+    )
+    partner_risk_adjustment_done_grp_ids = fields.Many2many(
+        string="Allowed To Validate Partner Risk Adjustment",
+        comodel_name="res.groups",
+        related="company_id.partner_risk_adjustment_done_grp_ids",
+        store=False,
+    )
+    partner_risk_adjustment_cancel_grp_ids = fields.Many2many(
+        string="Allowed To Cancel Partner Risk Adjustment",
+        comodel_name="res.groups",
+        related="company_id.partner_risk_adjustment_cancel_grp_ids",
+        store=False,
+    )
+    partner_risk_adjustment_restart_grp_ids = fields.Many2many(
+        string="Allowed To Restart Partner Risk Adjustment",
+        comodel_name="res.groups",
+        related="company_id.partner_risk_adjustment_restart_grp_ids",
+        store=False,
+    )
