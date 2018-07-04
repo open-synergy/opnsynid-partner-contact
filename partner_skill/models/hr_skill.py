817d29dc6151f7571184cb74fcec08858cd43b18
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-04 15:46:21 +0700

    [8.0.1.0.0][partner_skill]
    [8.0.1.0.0][partner_skill] Revisions
    
    [8.0.1.0.0][partner_skill] Revisions

---
+++ partner_skill/models/hr_skill.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class Skill(models.Model):
+    _inherit = 'hr.skill'
+
+    partner_ids = fields.Many2many(
+        comodel_name='res.partner',
+        relation='skill_partner_rel',
+        column1='skill_id',
+        column2='partner_id',
+        string='Partner(s)'
+    )
