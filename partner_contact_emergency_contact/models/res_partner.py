76fdd0b2d529ed99956a66405044defd0f442195
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-09 12:09:59 +0700

    [12.0.1.0.0] partner_contact_emergency_contact
    Migration history:
    * [8.0.1.0.0][partner_emergency_contact]

---
+++ partner_contact_emergency_contact/models/res_partner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    emergency_contact_ids = fields.Many2many(
+        string='Emergency Contacts',
+        comodel_name='res.partner',
+        relation='rel_partner_emergency_contact',
+        column1='partner_id',
+        column2='emergency_contact_id',
+        domain=[
+            ('is_company', '=', False),
+        ])
