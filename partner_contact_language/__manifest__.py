9e3383be94adb2f355d362db2e615cbcf65abee5
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-09 17:50:36 +0700

    [12.0.1.0.0] partner_contact_language
    Migration history:
    * [8.0.1.0.0][partner_language]
    * [8.0.1.1.0] partner_language
    * [8.0.1.1.1] partner_language

---
+++ partner_contact_language/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Contact Language Skills",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_view.xml",
+    ],
+}
