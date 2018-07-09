481d89da72ca60464510f8001b27ec7035b3ffef
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-09 17:25:43 +0700

    [8.0.1.0.0][partner_job_recommendation]

---
+++ partner_job_recommendation/models/res_partner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    job_recommendation_ids = fields.Many2many(
+        string="Job Recommendations",
+        comodel_name="res.partner",
+        relation="rel_partner_job_recommendation",
+        column1="partner_id",
+        column2="job_recommendation_id",
+        domain=[
+            ("is_company", "=", False),
+        ])
