5ef053bf8666390785e235e84102582e206da0fc
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-09 12:09:59 +0700

    [12.0.1.0.0] partner_contact_emergency_contact
    Migration history:
    * [8.0.1.0.0][partner_emergency_contact]

---
+++ partner_contact_emergency_contact/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Contact's Emergency Contact",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "contacts"
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
