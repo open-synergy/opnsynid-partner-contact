e98ec6b0f16cbafc029b57b8089808ae5859dcc3
Andhitia Rama <andhitia.r@gmail.com>
2018-08-22 09:21:29 +0700

    [8.0.1.0.0] partner_app_field_of_work

---
+++ partner_app_field_of_work/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Management Application - Field of Work",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "partner_app",
+        "partner_field_of_work",
+        ],
+    "data": [
+        "views/res_partner_view.xml",
+        ],
+}
