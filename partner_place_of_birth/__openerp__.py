0eeeaa72fd999243bf4b044cc98f3bbc6e737ea5
Andhitia Rama <andhitia.r@gmail.com>
2018-09-19 11:01:53 +0700

    [8.0.1.0.0] partner_place_of_birth

---
+++ partner_place_of_birth/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Contact Place of Birth",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "partner_contact_birthdate",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
