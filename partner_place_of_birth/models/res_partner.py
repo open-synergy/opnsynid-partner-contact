4bd7b57591d519dd54a025b8e346f6a454596260
Andhitia Rama <andhitia.r@gmail.com>
2018-09-19 11:01:53 +0700

    [8.0.1.0.0] partner_place_of_birth

---
+++ partner_place_of_birth/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    place_of_birth = fields.Char(
+        string="Place of Birth",
+    )
