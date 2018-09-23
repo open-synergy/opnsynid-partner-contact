e796a0220ec84b3497ff0670dad3839ff9a65ad2
Andhitia Rama <andhitia.r@gmail.com>
2018-09-23 22:41:13 +0700

    [8.0.1.3.0] partner_education_level
    * Add diploma information on partner

--- partner_education_level/__openerp__.py
+++ partner_education_level/__openerp__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Partner Education Level",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.3.0",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
