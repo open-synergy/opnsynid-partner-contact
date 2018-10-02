9f5fe3cc3c05fa0c96005d50fe6f5fcba16af18c
Andhitia Rama <andhitia.r@gmail.com>
2018-10-02 19:41:58 +0700

    [8.0.4.1.0] partner_financial_risk_policy

--- partner_financial_risk_policy/__openerp__.py
+++ partner_financial_risk_policy/__openerp__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Partner Risk Configuration Policy",
-    "version": "8.0.4.0.2",
+    "version": "8.0.4.1.0",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
