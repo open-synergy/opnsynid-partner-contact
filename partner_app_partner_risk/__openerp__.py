696cfeeb4c3a778b0fc7d16b2494d30ca0af0202
Andhitia Rama <andhitia.r@gmail.com>
2018-10-02 20:18:14 +0700

    [8.0.2.0.0] partner_app_partner_risk
    * Change dependency from partner_app into partner_app_account

--- partner_app_partner_risk/__openerp__.py
+++ partner_app_partner_risk/__openerp__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Partner App - Partner Risk Extension",
-    "version": "8.0.1.0.0",
+    "version": "8.0.2.0.0",
     "category": "Partner",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -11,7 +12,7 @@
     "installable": True,
     "auto_install": True,
     "depends": [
-        "partner_app",
+        "partner_app_account",
         "partner_financial_risk",
     ],
     "data": [
