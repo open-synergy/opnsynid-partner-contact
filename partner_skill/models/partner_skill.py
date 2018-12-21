f618446e7728176c3cc48d62a9e850fa34d6c9af
Andhitia Rama <andhitia.r@gmail.com>
2018-12-21 20:02:02 +0700

    [8.0.1.1.1] partner_skill
    * No duplicate skill for same partner

--- partner_skill/models/partner_skill.py
+++ partner_skill/models/partner_skill.py
@@ -2,7 +2,8 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from openerp import fields, models, api, _
+from openerp.exceptions import Warning as UserError
 
 
 class PartnerSkill(models.Model):
@@ -25,3 +26,20 @@ class PartnerSkill(models.Model):
         comodel_name="hr.skill_level",
         required=True,
     )
+
+    @api.constrains(
+        "partner_id",
+        "skill_id",
+    )
+    def _check_no_duplicate_skill(self):
+        obj_skill = self.env["partner.skill"]
+        for skill in self:
+            criteria = [
+                ("id", "!=", skill.id),
+                ("partner_id", "=", skill.partner_id.id),
+                ("skill_id", "=", skill.skill_id.id)
+            ]
+            result = obj_skill.search_count(criteria)
+            if result > 0:
+                msg = _("No duplicate skill")
+                raise UserError(msg)
