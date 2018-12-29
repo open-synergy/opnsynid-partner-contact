3cf00a4d64243df62ca2fb7c93def539be6e8b25
Andhitia Rama <andhitia.r@gmail.com>
2018-12-30 00:33:27 +0700

    [8.0.1.0.0] partner_contact_ethnicity

---
+++ partner_contact_ethnicity/models/partner_ethnicity.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class PartnerEthnicity(models.Model):
+    _name = "partner.ethnicity"
+    _description = "Ethnicity"
+
+    name = fields.Char(
+        string="Ethnicity",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+    )
+    note = fields.Text(
+        string="Note",
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
