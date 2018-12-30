7264a1e4e2ddde7c7c87e3431fa661765d7b3fa3
Andhitia Rama <andhitia.r@gmail.com>
2018-12-30 08:25:10 +0700

    [8.0.1.0.0] partner_app_ethnicity

---
+++ partner_app_ethnicity/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Management Application - Ethnicity",
+    "version": "8.0.1.0.0",
+    "category": "Partner",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "partner_app",
+        "partner_contact_ethnicity",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+}
