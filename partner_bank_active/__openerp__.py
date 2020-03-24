7f3575bcdfcd494ae8551aaac9a82dd328dd7bae
Andhitia Rama <andhitia.r@gmail.com>
2020-03-25 06:23:54 +0700

    [8.0.1.0.0] partner_bank_active
    * QC

--- partner_bank_active/__openerp__.py
+++ partner_bank_active/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Bank Active",
     "version": "8.0.1.0.0",
     "category": "Partner Management",
     "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
