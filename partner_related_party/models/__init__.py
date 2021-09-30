4e7eaa4dcb0fa54b23cb2268a4661e44afd899f6
Nurazmi <azmimr67@gmail.com>
2021-09-30 07:35:30 +0700

    [8.0.1.0.0] partner_related_party Task T003946 dan revisi pylint

---
+++ partner_related_party/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    res_partner_related_party,
+    res_partner_related_party_transaction_type,
+    res_partner_related_party_relationship_type,
+    res_partner,
+)
---
+++ partner_related_party/models/res_partner_related_party_transaction_type.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class ResPartnerRelatedPartyTransactionType(models.Model):
+    _name = "res.partner.related_party_transaction_type"
+    _description = "Related Party Transaction Type"
+    _order = "sequence, id"
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+        required=True,
+    )
+    sequence = fields.Integer(
+        string="Sequence",
+        required=True,
+        default=5,
+    )
+    description = fields.Text(
+        string="Description",
+    )
