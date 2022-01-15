19a90a6b9f5c7735da635e027c727a881c006bb2
Nurazmi <azmimr67@gmail.com>
2022-01-15 09:44:02 +0700

    [partner_company_legal_name] 11.0.1.0.0
    * Task T/22/T005560 Pembuatan Awal menambal fiel legal_name

---
+++ partner_company_legal_name/models/res_partner.py
@@ -0,0 +1,30 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    """Add legal_name"""
+
+    _inherit = "res.partner"
+
+    @api.depends(
+        "manual_legal_name",
+        "name",
+    )
+    def _compute_legal_name(self):
+        for record in self:
+            legal_name = record.name
+            if record.manual_legal_name:
+                legal_name = record.manual_legal_name
+            record.legal_name = legal_name
+
+    manual_legal_name = fields.Char(
+        string="Manual Legal Name",
+        required=False,
+    )
+    legal_name = fields.Char(
+        string="Legal Name", compute="_compute_legal_name", required=False, store=False
+    )
